06630e8456148e4f05cfb370824beddc15086ea7
Loft <animesenpai.kimi@gmail.com>
2026-05-25 01:11:24 +0500

    fix2

--- install.sql
+++ install.sql
@@ -5,7 +5,7 @@ CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     login VARCHAR(50) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
-    fio VARCHAR(255) NOT NULL,
+    full_name VARCHAR(255) NOT NULL,
     phone VARCHAR(20) NOT NULL,
     email VARCHAR(100) NOT NULL
 );
