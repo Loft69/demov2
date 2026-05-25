5df5406ffb63f10592d5bd7ca6e0d69c60868967
Loft <animesenpai.kimi@gmail.com>
2026-05-26 00:05:52 +0500

    db.php

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
-$db = mysqli_connect('localhost', 'root', '', 'user7');
+$db = mysqli_connect('localhost', 'user12', 'user12', 'user12');
 mysqli_set_charset($db, 'utf8');
 session_start();
 ?>
